781cebd716387eefe0231170963d6bba2e0763d5
joaogablira <joaogab.lira24@gmail.com>
2025-02-17 15:51:54 -0400

    aula02

---
+++ aulas/aula02/exemplo01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 1 </title>
+</head>
+<body>
+
+    <?php
+        $nome = "JG";
+    ?>
+   <p>
+        Bea bea 
+   </p> 
+
+   <?php 
+        echo "<p> Olá Mundo </p>";
+        echo "<p> Eu sou o $nome </p> ";
+        echo "<p> Olha o ".$nome." </p>";
+    ?>
+</body>
+</html>
