a1a9b986bfbea34e7805788937a4b3876c84c658
João Gabriel Tavares de Lira <159500924+joaogablira@users.noreply.github.com>
2025-05-13 22:39:20 -0300

    Update exercicio02.php

--- aulas/exercicio_lampada/exercicio02.php
+++ aulas/exercicio_lampada/exercicio02.php
@@ -1,81 +1,51 @@
 <?php
     include_once 'Carro.class.php';
-    
+
     $car1 = new Carro();
     $car2 = new Carro();
     $car3 = new Carro();
     $car4 = new Carro();
     $car5 = new Carro();
 
-    
+
     //Carro 1;
 
-    $car1->Marca = "Toyota";
-    $car1->Modelo = "Corolla";
+    $car1->Marca = "Chevrolet";
+    $car1->Modelo = "Onix";
     $car1->Combustivel =  "Gasolina";
-    $car1->Portas = 5;
-    $car1->Capacidade_tanque = 60;
+    $car1->Portas = 4;
+    $car1->Capacidade_tanque = 50;
 
     //Carro 2;
 
-    $car2->Marca = "Toyota";
-    $car2->Modelo = "Corolla";
-    $car2->Combustivel =  "Gasolina";
-    $car2->Portas = 5;
-    $car2->Capacidade_tanque = 60;
+    $car2->Marca = "Volkswagen";
+    $car2->Modelo = "Gol";
+    $car2->Combustivel =  "Etanol";
+    $car2->Portas = 4;
+    $car2->Capacidade_tanque = 55;
 
     //Carro 3;
 
-    $car3->Marca = "Toyota";
-    $car3->Modelo = "Corolla";
+    $car3->Marca = "Honda";
+    $car3->Modelo = "Civic";
     $car3->Combustivel =  "Gasolina";
-    $car3->Portas = 5;
-    $car3->Capacidade_tanque = 60;
+    $car3->Portas = 4;
+    $car3->Capacidade_tanque = 47;
 
     //Carro 4;
 
     $car4->Marca = "Toyota";
     $car4->Modelo = "Corolla";
     $car4->Combustivel =  "Gasolina";
-    $car4->Portas = 5;
-    $car4->Capacidade_tanque = 60;
+    $car4->Portas = 4;
+    $car4->Capacidade_tanque = 50;
 
     //Carro 5;
 
-    $car5->Marca = "Toyota";
-    $car5->Modelo = "Corolla";
-    $car5->Combustivel =  "Gasolina";
-    $car5->Portas = 5;
-    $car5->Capacidade_tanque = 60;
+    $car5->Marca = "Ford";
+    $car5->Modelo = "Fiesta";
+    $car5->Combustivel =  "Etanol";
+    $car5->Portas = 4;
+    $car5->Capacidade_tanque = 47;
 
 ?>
-
-//Marca: Chevrolet
-Modelo: Onix
-Combustível: Gasolina
-Portas: 4
-Capacidade do tanque: 50 litros
-
-Marca: Volkswagen
-Modelo: Gol
-Combustível: Etanol
-Portas: 4
-Capacidade do tanque: 55 litros
-
-Marca: Honda
-Modelo: Civic
-Combustível: Gasolina
-Portas: 4
-Capacidade do tanque: 47 litros
-
-Marca: Toyota
-Modelo: Corolla
-Combustível: Gasolina
-Portas: 4
-Capacidade do tanque: 50 litros
-
-Marca: Ford
-Modelo: Fiesta
-Combustível: Etanol
-Portas: 4
-Capacidade do tanque: 47 litros
