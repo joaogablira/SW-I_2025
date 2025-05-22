98d8e9c94fec96bbcb31ee845ffdfeeb6b119791
João Gabriel Tavares de Lira <159500924+joaogablira@users.noreply.github.com>
2025-05-22 08:14:53 -0300

    Update coiso.php

--- aulas/aula11/coiso.php
+++ aulas/aula11/coiso.php
@@ -4,5 +4,7 @@
     $produto1 = new Produto("Mouse", 150.00, 10);
     
     $produto1->adicionarEstoque(5);
-    $produto1->mostrarDetalhes(); 
+    $produto1->mostrarDetalhes();
+    $produto1->RemoverEstoque(3);
+    $produto1->mostrarDetalhes();
 ?>
